fcd285c40ef8bca79747abca5461fe65ce40e2f7
IT18117974 <it18117974@my.sliit.lk>
2021-10-21 23:51:34 +0530

    IRMS: frontend and backend added separately

---
+++ react-jwt-auth/src/services/user.service.js
@@ -0,0 +1,26 @@
+//Data Service
+
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/api/test/';
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
+  }
+
+  getUserBoard() {
+    return axios.get(API_URL + 'user', { headers: authHeader() });
+  }
+
+  getInternMgrBoard() {
+    return axios.get(API_URL + 'internMgr', { headers: authHeader() });
+  }
+
+  getAdminBoard() {
+    return axios.get(API_URL + 'admin', { headers: authHeader() });
+  }
+}
+
+export default new UserService();
